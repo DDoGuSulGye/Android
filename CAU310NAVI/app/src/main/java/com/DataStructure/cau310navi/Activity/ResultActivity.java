ddba2e8a9664d163b32eec09fdf5959de50a4b2b
권도경 <dkdk6638@gmail.com>
2018-12-09 02:28:35 +0900

    결과 UI 수정
    * 수직 이동 BoomTime 시간 추가 해야 함!
    * 경로 중 중복 나오는 것 수정 해야 함!
    * 계단과 엘베 뺴고  다 Fragment에서 지웠음!

--- CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Activity/ResultActivity.java
+++ CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Activity/ResultActivity.java
@@ -6,14 +6,9 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
-import android.os.Parcelable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ExpandableListView;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -24,16 +19,12 @@ import com.DataStructure.cau310navi.Data.HorizontalAlgorithm;
 import com.DataStructure.cau310navi.Data.StairNode;
 import com.DataStructure.cau310navi.Fragment.Elevator_Fragment;
 import com.DataStructure.cau310navi.Fragment.Stair_Fragment;
-import com.DataStructure.cau310navi.Fragment.Time_Fragment;
-import com.DataStructure.cau310navi.Fragment.Wait_Fragment;
 import com.DataStructure.cau310navi.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by dkdk6 on 2018-11-18.
@@ -45,8 +36,7 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
     FragmentTransaction tran;
     Elevator_Fragment elevator;
     Stair_Fragment stair;
-    Time_Fragment time;
-    Wait_Fragment wait;
+
 
     DataPool dataPool;
     HorizontalAlgorithm horizontalAlgorithm;
@@ -72,15 +62,13 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_result);
         bt1 = (ImageButton) findViewById(R.id.bt1);
         bt2 = (ImageButton) findViewById(R.id.bt2);
-        bt3 = (ImageButton) findViewById(R.id.bt3);
+
 
         bt1.setOnClickListener(this);
         bt2.setOnClickListener(this);
-        bt3.setOnClickListener(this);
         elevator = new Elevator_Fragment();
         stair = new Stair_Fragment();
-        time = new Time_Fragment();
-        wait = new Wait_Fragment();
+
 
         Intent intent = getIntent();
         setData(intent);
@@ -258,9 +246,6 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.bt2:
                 setFrag(1);
                 break;
-            case R.id.bt3:
-                setFrag(2);
-                break;
         }
     }
 
@@ -269,7 +254,6 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         tran = fm.beginTransaction();
         switch (n){
             case 0:
-                Toast toast = Toast.makeText(getApplicationContext(), "Fra1", Toast.LENGTH_LONG); toast.show();
                 Bundle bundle1 = new Bundle();
                 if(middleToEndElevator==null){
                     //경유지 없는 경우
@@ -296,18 +280,7 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
                     bundle2.putSerializable("LIST2", new DataHelper(middleToEndStair));
                 }
                 stair.setArguments(bundle2);
-                Toast toast2 = Toast.makeText(getApplicationContext(), "Fra2", Toast.LENGTH_LONG); toast2.show();
                 tran.replace(R.id.main_frame, stair);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
-            case 2:
-                Toast toast3 = Toast.makeText(getApplicationContext(), "Fra3", Toast.LENGTH_LONG); toast3.show();
-                tran.replace(R.id.main_frame, time);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
-                tran.commit();
-                break;
-            case 3:
-                Toast toast4 = Toast.makeText(getApplicationContext(), "Fra4", Toast.LENGTH_LONG); toast4.show();
-                tran.replace(R.id.main_frame, wait);  //replace의 매개변수는 (프래그먼트를 담을 영역 id, 프래그먼트 객체) 입니다.
                 tran.commit();
                 break;
         }
--- CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Fragment/Elevator_Fragment.java
+++ CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Fragment/Elevator_Fragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -30,115 +31,134 @@ public class Elevator_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_elevator, container, false);
         LinearLayout topLL = (LinearLayout)view.findViewById(R.id.elevator_main_layout); //parent view
-        LinearLayout subLL = (LinearLayout)view.findViewById(R.id.dynamicArea);
+        Double distance = 0.0;
+
         resultType= getArguments().getInt("Type");
         if(resultType==0){
-            //경유지 없는 경우
-            data1 = ((DataHelper)getArguments().getSerializable("LIST1")).getList();
             data1 = ((DataHelper)getArguments().getSerializable("LIST1")).getList(); //startToMiddleElevator
-            data2 = ((DataHelper)getArguments().getSerializable("LIST2")).getList(); //middleToEndElevator
+            for(int a = 0; a<data1.size(); a++){
+                distance+=Double.parseDouble(data1.get(a).get(data1.get(a).size()-1));
+            }
+            TextView totalDistance = new TextView(view.getContext());
+            totalDistance.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            totalDistance.setPadding(10, 10, 10, 10);
+            totalDistance.setTextSize(30);
+            distance = distance*7;
+            distance = distance/60;
+            totalDistance.setText("총 소요시간 : 약" +distance.intValue()+"분");
+            totalDistance.setTextColor(Color.parseColor("#000000"));
+            topLL.addView(totalDistance);
             for(int i=0; i<data1.size(); i++) {
-                TextView topTV1 = new TextView(view.getContext());
-                topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                topTV1.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                topTV1.setPadding(20, 10, 10, 10);
-                topTV1.setTextColor(Color.parseColor("#FF7200"));
-                topTV1.setTextSize(20);
-                if(i==data1.size()-1){
-                    topTV1.setText(data1.get(i).get(data1.get(i).size()-2)); //END
-                }else{
-                    topTV1.setText(data1.get(i).get(0)); //Start
-                }
-                topLL.addView(topTV1);
-                LinearLayout parentLL = new LinearLayout(view.getContext());
-                parentLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                parentLL.setOrientation(LinearLayout.HORIZONTAL);
-                for(int j=1; j<data1.get(i).size()-1; j++){
-                    TextView topTV2 = new TextView(view.getContext());
-                    topTV2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    topTV2.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                    topTV2.setPadding(20, 10, 10, 10);
-                    topTV2.setTextColor(Color.parseColor("#dddddd"));
-                    topTV2.setTextSize(13);
-                    if(data1.get(i).get(j).length()==1){
-                        topTV2.setText(data1.get(0).get(j)+"구역"); //Start
+                for(int j=0; j<data1.get(i).size()-1; j++){
+                    LinearLayout mainLL = new LinearLayout(view.getContext());
+                    TextView topTV1 = new TextView(view.getContext());
+                    ImageView mainImage = new ImageView(view.getContext());
+                    mainLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    mainLL.setOrientation(LinearLayout.HORIZONTAL);
+                    topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    topTV1.setPadding(10, 10, 10, 10);
+
+                    topTV1.setTextColor(Color.parseColor("#000000"));
+                    if((j==0)&&(i==0)||(i== data1.size()-1)&&(j==data1.get(i).size()-2)){
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(70, 70));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.mainnode));
+                        mainImage.setPadding(10,5,5,5);
+                        topTV1.setTextSize(20);
+                        topTV1.setText(data1.get(i).get(j));
                     }else{
-                        topTV2.setText(data1.get(0).get(j)); //Start
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(60, 60));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.subroot));
+                        mainImage.setPadding(30,5,5,5);
+                        topTV1.setTextSize(15);
+                        topTV1.setText(data1.get(i).get(j)+" 구역");
                     }
-                    parentLL.addView(topTV2);
+                    mainLL.addView(mainImage);
+                    mainLL.addView(topTV1);
+                    topLL.addView(mainLL);
+
                 }
-                topLL.addView(parentLL);
             }
         }else{
+            ArrayList<String> routeList = new ArrayList<>();
             data1 = ((DataHelper)getArguments().getSerializable("LIST1")).getList(); //startToMiddleElevator
             data2 = ((DataHelper)getArguments().getSerializable("LIST2")).getList(); //middleToEndElevator
-            for(int i=0; i<data1.size(); i++) {
-                TextView topTV1 = new TextView(view.getContext());
-                topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                topTV1.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                topTV1.setPadding(20, 10, 10, 10);
-                topTV1.setTextColor(Color.parseColor("#FF7200"));
-                topTV1.setTextSize(20);
-                if(i==data1.size()-1){
-                    topTV1.setText(data1.get(i).get(data1.get(i).size()-2)); //END
-                }else{
-                    topTV1.setText(data1.get(i).get(0)); //Start
-                }
-                topLL.addView(topTV1);
-                LinearLayout parentLL = new LinearLayout(view.getContext());
-                parentLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                parentLL.setOrientation(LinearLayout.HORIZONTAL);
-                for(int j=1; j<data1.get(i).size()-1; j++){
-                    TextView topTV2 = new TextView(view.getContext());
-                    topTV2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    topTV2.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                    topTV2.setPadding(20, 10, 10, 10);
-                    topTV2.setTextColor(Color.parseColor("#dddddd"));
-                    topTV2.setTextSize(13);
-                    if(data1.get(i).get(j).length()==1){
-                        topTV2.setText(data1.get(0).get(j)+"구역"); //Start
-                    }else{
-                        topTV2.setText(data1.get(0).get(j)); //Start
-                    }
-                    parentLL.addView(topTV2);
-                }
-                topLL.addView(parentLL);
+            //1. 시간 계산
+            for(int a = 0; a<data1.size(); a++){
+                distance+=Double.parseDouble(data1.get(a).get(data1.get(a).size()-1));
             }
-            for(int i=0; i<data2.size(); i++) {
-                TextView topTV1 = new TextView(view.getContext());
-                topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                topTV1.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                topTV1.setPadding(20, 10, 10, 10);
-                topTV1.setTextColor(Color.parseColor("#FF7200"));
-                topTV1.setTextSize(20);
-                if(i==data2.size()-1){
-                    topTV1.setText(data2.get(i).get(data2.get(i).size()-2)); //END
-                }else{
-                    topTV1.setText(data2.get(i).get(0)); //Start
-                }
-                topLL.addView(topTV1);
-                LinearLayout parentLL = new LinearLayout(view.getContext());
-                parentLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                parentLL.setOrientation(LinearLayout.HORIZONTAL);
-                for(int j=1; j<data2.get(i).size()-1; j++){
-                    TextView topTV2 = new TextView(view.getContext());
-                    topTV2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    topTV2.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-                    topTV2.setPadding(20, 10, 10, 10);
-                    topTV2.setTextColor(Color.parseColor("#dddddd"));
-                    topTV2.setTextSize(13);
-                    if(data2.get(i).get(j).length()==1){
-                        topTV2.setText(data2.get(0).get(j)+"구역"); //Start
+            for(int b = 0; b<data2.size(); b++){
+                distance+= Double.parseDouble(data2.get(b).get(data2.get(b).size()-1));
+            }
+            TextView totalDistance = new TextView(view.getContext());
+            totalDistance.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            totalDistance.setPadding(10, 10, 10, 10);
+            totalDistance.setTextSize(30);
+            distance = distance*7;
+            totalDistance.setTextColor(Color.parseColor("#000000"));
+            distance = distance/60;
+            totalDistance.setText("총 소요시간 : 약 "+distance.intValue()+"분");
+            topLL.addView(totalDistance);
+            //2. 경로 넣기 start to middle
+            for(int i=0; i<data1.size(); i++) {
+                for(int j=0; j<data1.get(i).size()-1; j++){
+                    LinearLayout mainLL = new LinearLayout(view.getContext());
+                    TextView topTV1 = new TextView(view.getContext());
+                    ImageView mainImage = new ImageView(view.getContext());
+                    mainLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    mainLL.setOrientation(LinearLayout.HORIZONTAL);
+                    topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    topTV1.setPadding(10, 10, 10, 10);
+
+                    topTV1.setTextColor(Color.parseColor("#000000"));
+                    if((j==0)&&(i==0)||(i== data1.size()-1)&&(j==data1.get(i).size()-2)){
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(70, 70));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.mainnode));
+                        mainImage.setPadding(10,5,5,5);
+                        topTV1.setTextSize(20);
+                        topTV1.setText(data1.get(i).get(j));
                     }else{
-                        topTV2.setText(data2.get(0).get(j)); //Start
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(60, 60));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.subroot));
+                        mainImage.setPadding(30,5,5,5);
+                        topTV1.setTextSize(15);
+                        topTV1.setText(data1.get(i).get(j)+" 구역");
                     }
-                    parentLL.addView(topTV2);
+                    mainLL.addView(mainImage);
+                    mainLL.addView(topTV1);
+                    topLL.addView(mainLL);
                 }
-                topLL.addView(parentLL);
+            }
+            //midle to end
+            for(int i=0; i<data2.size(); i++) {
+                for(int j=1; j<data2.get(i).size()-1; j++){
+                    LinearLayout mainLL = new LinearLayout(view.getContext());
+                    TextView topTV1 = new TextView(view.getContext());
+                    ImageView mainImage = new ImageView(view.getContext());
+                    mainLL.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    mainLL.setOrientation(LinearLayout.HORIZONTAL);
+                    topTV1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    topTV1.setPadding(10, 10, 10, 10);
+                    topTV1.setTextColor(Color.parseColor("#000000"));
+                    if((j==0)&&(i==0)||(i== data2.size()-1)&&(j==data2.get(i).size()-2)){
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(70, 70));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.mainnode));
+                        mainImage.setPadding(10,5,5,5);
+                        topTV1.setTextSize(20);
+                        topTV1.setText(data2.get(i).get(j));
+                    }else{
+                        mainImage.setLayoutParams(new LinearLayout.LayoutParams(60, 60));
+                        mainImage.setImageDrawable(view.getResources().getDrawable(R.drawable.subroot));
+                        mainImage.setPadding(30,5,5,5);
+                        topTV1.setTextSize(15);
+                        topTV1.setText(data2.get(i).get(j)+" 구역");
+                    }
+
+                    mainLL.addView(mainImage);
+                    mainLL.addView(topTV1);
+                    topLL.addView(mainLL);
+                }
             }
         }
         return view;
     }
-
-
 }
