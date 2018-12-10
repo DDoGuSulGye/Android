fe3128c514d184c6e358f7c8a0cc8064bd6b7a25
Miji Park <ppmj789@naver.com>
2018-12-10 15:20:47 +0900

    loading,main,icon UI

--- CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Activity/MainActivity.java
+++ CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/Activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageButton;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.DataStructure.cau310navi.LoadingActivity;
 import com.DataStructure.cau310navi.R;
 
 import java.util.ArrayList;
--- CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/LoadingActivity.java
+++ CAU310NAVI/app/src/main/java/com/DataStructure/cau310navi/LoadingActivity.java
@@ -1,11 +1,16 @@
 package com.DataStructure.cau310navi;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.DataStructure.cau310navi.Activity.MainActivity;
 import com.DataStructure.cau310navi.Activity.ResultActivity;
+
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -13,6 +18,7 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
         Intent i = new Intent(LoadingActivity.this, MainActivity.class);
         startActivity(i);
         finish();
